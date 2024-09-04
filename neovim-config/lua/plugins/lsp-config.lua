623492ced85940b959075d94d17ea646ce5cc7a2
Michael Roderick <rodericktech@users.noreply.github.com>
2024-09-04 17:03:21 -0400

    Update core configs

--- neovim-config/lua/plugins/lsp-config.lua
+++ neovim-config/lua/plugins/lsp-config.lua
@@ -58,6 +58,9 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+      -- Open eslint messages
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 		end,
 	},
 }
